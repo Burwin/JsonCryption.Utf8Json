7a77cf6825fd32e1a197be533451a3318954b901
burwin <19492392+Burwin@users.noreply.github.com>
2020-03-04 19:53:55 -0500

    make serializable to fix sonar code smell

--- src/Utf8Json.FLE/ConstructorResolutionException.cs
+++ src/Utf8Json.FLE/ConstructorResolutionException.cs
@@ -2,10 +2,23 @@
 
 namespace JsonCryption.Utf8Json
 {
+    /// <summary>
+    /// Exception thrown when an expected constructor for a given type cannot be found
+    /// </summary>
+    [Serializable]
     public sealed class ConstructorResolutionException : Exception
     {
+        /// <summary>
+        /// Create a new instance from the provided Type
+        /// </summary>
+        /// <param name="type">The Type for which an expected constructor cannot be found</param>
         public ConstructorResolutionException(Type type)
             : base($"No suitable constructor found for type {type.Name}")
+        {
+        }
+
+        private ConstructorResolutionException(System.Runtime.Serialization.SerializationInfo serializationInfo, System.Runtime.Serialization.StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
         {
         }
     }
